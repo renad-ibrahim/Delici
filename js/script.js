9123504b0132b66439f01d98ee5c16e517bf2129
renad <renadibrahim010@gmail.com>
2022-12-01 19:35:19 +0200

    delici is a resturant website

---
+++ js/script.js
@@ -0,0 +1,47 @@
+var swiper = new Swiper(".team-swiper", {
+    navigation: {
+      nextEl: ".swiper-button-nexts",
+      prevEl: ".swiper-button-prevs",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    // Responsive breakpoints
+    breakpoints: {
+      250: {
+        slidesPerView: 1.2,
+        spaceBetween: 80
+      },
+      // when window width is >= 320px
+      300: {
+        slidesPerView: 1.2,
+        spaceBetween: 0
+      },
+      // when window width is >= 480px
+      400: {
+        slidesPerView: 1.5,
+        spaceBetween: 20
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 40
+      },
+      840: {
+        slidesPerView: 3,
+        spaceBetween: 40
+      },
+      1150: {
+        slidesPerView: 4,
+        spaceBetween: 40
+      }
+    }
+  });  
+
+
+
+
